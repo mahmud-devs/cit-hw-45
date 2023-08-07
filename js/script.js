2af89ba3a7204525f881da11e190fd41a494e335
mahmud-devs <mahmudulislam0999@gmail.com>
2023-08-07 06:04:49 +0600

    bottom to top

--- js/script.js
+++ js/script.js
@@ -29,26 +29,29 @@ $(function(){
     $('body').attr('class', 'purple');
     }
 
-    // $('.setting').click(function () {
-    //     $('.color').fadeOut(600);
-    // });
-    // $('.setting').click(function () {
-    //     $('.color').fadeIn(600);
-    // });
 
-    // $(window).scroll(function(){
-    //     let scroll = $(this).scrollTop()
-    //     console.log(scroll);
-    //     if(scroll > 400){
-    //         $(".bottom_to_top").fadeIn(600)
-    //     }else{
-    //         $(".bottom_to_top").fadeOut(600)
-    //     }
-    //     if(scroll > 200){
-    //         $(".nav").addClass("fixed")
-    //     }else{
-    //         $(".nav").removeClass("fixed")
-    //     }
-    // })
+  
+});
 
-})
+
+$(function(){
+    // bottom to top
+    $(".bottom_to_top").click(function(){
+        $("body,html").animate({scrollTop:0},1000)
+    });
+    $(window).scroll(function(){
+        let scroll = $(this).scrollTop()
+        console.log(scroll);
+        if(scroll > 800){
+            $(".bottom_to_top").fadeIn(600)
+        }else{
+            $(".bottom_to_top").fadeOut(600)
+        }
+        // if(scroll > 200){
+        //     $(".nav").addClass("fixed")
+        // }else{
+        //     $(".nav").removeClass("fixed")
+        // }
+    })
+
+});
